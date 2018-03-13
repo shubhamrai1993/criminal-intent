fc461e589f6e9ac28bdf1857c5ce5f63a24cdfad
Shubham Rai <shubham.rai@shop101.com>
2018-03-13 18:33:52 +0530

    Removed the code related to mCrimes in CrimeLab

--- app/src/main/java/com/tanuki/criminalintent/CrimeLab.java
+++ app/src/main/java/com/tanuki/criminalintent/CrimeLab.java
@@ -14,7 +14,6 @@ import java.util.UUID;
 public class CrimeLab {
     private static CrimeLab sCrimeLab;
 
-    private List<Crime> mCrimes;
     private Context mContext;
     private SQLiteDatabase mDatabase;
 
@@ -29,7 +28,6 @@ public class CrimeLab {
         mContext = context.getApplicationContext();
         mDatabase = new CrimeBaseHelper(mContext)
                 .getWritableDatabase();
-        mCrimes = new ArrayList<>();
         /*for (int i = 0; i < 1000; i++) {
             Crime crime = new Crime();
             crime.setTitle("Crime #" + i);
@@ -39,19 +37,14 @@ public class CrimeLab {
     }
 
     public void addCrime(Crime c) {
-        mCrimes.add(c);
+
     }
 
     public List<Crime> getCrimes() {
-        return mCrimes;
+        return new ArrayList<>();
     }
 
     public Crime getCrime(UUID id) {
-        for (Crime crime : mCrimes) {
-            if (crime.getId().equals(id)) {
-                return crime;
-            }
-        }
         return null;
     }
 }
